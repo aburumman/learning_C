92b73fab60cf87332301961e86198800d333b878
Alayande Mustapha <Alayande Mustapha>
2022-06-22 23:34:09 +0100

    Nice

--- sahi.c
+++ sahi.c
@@ -1,13 +1,15 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-void sayHi(char name[])
-{
-    printf("Hello %s \n", name);
-}
+void sayHi();
 
 int main(void)
 {
     sayHi("Mike");
     return 0;
+}
+
+void sayHi(char name[])
+{
+    printf("Hello %s \n", name);
 }
